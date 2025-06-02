62e563b40121603c73bdc599f78c307e07ffe1b7
brian7346 <doctorbrian97@gmail.com>
2025-06-02 15:05:51 +0400

    init

---
+++ public/react.js
@@ -0,0 +1,6 @@
+// Подключаем React
+document.write('<script src="https://unpkg.com/react@18/umd/react.development.js"></script>');
+// Подключаем React DOM
+document.write('<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>');
+// Подключаем Babel
+document.write('<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>');
